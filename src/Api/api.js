801d311e064095749ae34a06a0a2b6dec1a07224
prsandeep <149871217+prsandeep@users.noreply.github.com>
2024-09-19 14:22:04 +0530

    Update api.js

--- src/Api/api.js
+++ src/Api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const DEPLOYED='http://cryptoxchange.eu-north-1.elasticbeanstalk.com/'
 const LOCALHOST='http://localhost:8080'
 
-export const API_BASE_URL = LOCALHOST
+export const API_BASE_URL = DEPLOYED
 
 const api = axios.create({
   baseURL: API_BASE_URL,
